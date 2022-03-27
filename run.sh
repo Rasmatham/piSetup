e1c2aac6b84ffa301b11e0224f445e66aae687fc
RasMatHam <61523831+Rasmatham@users.noreply.github.com>
2022-03-27 15:47:46 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,28 +3,13 @@ sudo apt-get update
 sudo apt-get -y dist-upgrade
 
 #get script to install discord bots and run
-read -p "get script to install discord bots and run? " -n 1 -r
-echo    # (optional) move to a new line
-if [[ $REPLY =~ ^[Yy]$ ]]
-then
-    curl https://raw.githubusercontent.com/Rasmatham/Ras-s-Discord-Bots/main/piSetup.sh -O
-    bash piSetup.sh $1
-fi
+curl https://raw.githubusercontent.com/Rasmatham/Ras-s-Discord-Bots/main/piSetup.sh -O
+bash piSetup.sh $1
 
 #get script to install pihole and run
-read -p "get script to install pihole and run? " -n 1 -r
-echo    # (optional) move to a new line
-if [[ $REPLY =~ ^[Yy]$ ]]
-then
-    curl https://raw.githubusercontent.com/Rasmatham/piSetup/main/pihole.sh -O
-    bash pihole.sh
-fi
+curl https://raw.githubusercontent.com/Rasmatham/piSetup/main/pihole.sh -O
+bash pihole.sh
 
 #get script to install retropie and run
-read -p "get script to install retropie and run? " -n 1 -r
-echo    # (optional) move to a new line
-if [[ $REPLY =~ ^[Yy]$ ]]
-then
-    curl https://raw.githubusercontent.com/Rasmatham/piSetup/main/retropie.sh -O
-    bash retropie.sh
-fi
+curl https://raw.githubusercontent.com/Rasmatham/piSetup/main/retropie.sh -O
+bash retropie.sh
