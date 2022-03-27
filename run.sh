98f4f8f1e029254fe675764a891233f14b0053ed
RasMatHam <61523831+Rasmatham@users.noreply.github.com>
2022-03-27 14:21:46 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,10 +3,10 @@ sudo apt-get update
 sudo apt-get -y dist-upgrade
 
 #get script to install discord bots and run
-curl -s https://raw.githubusercontent.com/Rasmatham/Ras-s-Discord-Bots/main/piSetup.sh -o
+curl -s https://raw.githubusercontent.com/Rasmatham/Ras-s-Discord-Bots/main/piSetup.sh -o piSetup.sh
 
 #get script to install pihole and run
-curl -s https://raw.githubusercontent.com/Rasmatham/piSetup/main/pihole.sh -o
+curl -s https://raw.githubusercontent.com/Rasmatham/piSetup/main/pihole.sh -o pihole.sh
 
 #get script to install retropie and run
-curl -s https://raw.githubusercontent.com/Rasmatham/piSetup/main/retropie.sh -o
+curl -s https://raw.githubusercontent.com/Rasmatham/piSetup/main/retropie.sh -o retropie.sh
