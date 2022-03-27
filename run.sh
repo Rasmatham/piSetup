9d47368d5dd4687cd47460fd49f3b671a85a02e0
RasMatHam <61523831+Rasmatham@users.noreply.github.com>
2022-03-27 14:15:40 +0200

    first commit

---
+++ run.sh
@@ -0,0 +1,12 @@
+# make sure everything is up to date bfore starting
+sudo apt-get update
+sudo apt-get -y dist-upgrade
+
+#get script to install discord bots and run
+curl -s https://raw.githubusercontent.com/Rasmatham/Ras-s-Discord-Bots/main/piSetup.sh -o
+
+#get script to install pihole and run
+curl -s https://raw.githubusercontent.com/Rasmatham/piSetup/main/pihole.sh -o
+
+#get script to install retropie and run
+curl -s https://raw.githubusercontent.com/Rasmatham/piSetup/main/retropie.sh -o
