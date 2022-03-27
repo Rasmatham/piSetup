f428075fde9295050e6cf58b1d55df5503583e64
RasMatHam <61523831+Rasmatham@users.noreply.github.com>
2022-03-27 16:19:55 +0200

    ghjdhjtd

--- pihole/pihole.sh
+++ pihole/pihole.sh
@@ -1,4 +1,6 @@
 curl -sSL https://raw.githubusercontent.com/pi-hole/pi-hole/master/automated%20install/basic-install.sh -O
-curl https://raw.githubusercontent.com/Rasmatham/Ras-s-Discord-Bots/main/setupVars.conf -O
-mv setupVars.conf.sh /etc/pihole/
+curl https://raw.githubusercontent.com/Rasmatham/piSetup/main/pihole/setupVars.conf -O
+sudo rm -rf /etc/pihole
+sudo mkdir /etc/pihole
+sudo mv setupVars.conf /etc/pihole/
 sudo bash basic-install.sh
