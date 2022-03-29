54bf00b9f5407aec653bf698b9aaab37d602db68
RasMatHam <61523831+Rasmatham@users.noreply.github.com>
2022-03-29 20:44:38 +0200

    gdjjjhtrshjrhr

--- pihole/pihole.sh
+++ pihole/pihole.sh
@@ -1,8 +1,8 @@
-cd
+cd /
 curl -sSL https://raw.githubusercontent.com/pi-hole/pi-hole/master/automated%20install/basic-install.sh -O
 curl https://raw.githubusercontent.com/Rasmatham/piSetup/main/pihole/setupVars.conf -O
 sudo rm -rf /etc/pihole
 sudo mkdir /etc/pihole
 sudo mv setupVars.conf /etc/pihole/
 sudo bash basic-install.sh --unattended
-cd
+cd /
--- retropie/retropie.sh
+++ retropie/retropie.sh
@@ -1,6 +1,6 @@
-cd
+cd /
 sudo apt install git lsb-release
 git clone --progress --verbose --depth=1 https://github.com/RetroPie/RetroPie-Setup.git
 cd RetroPie-Setup
 sudo ./retropie_packages.sh setup basic_install
-cd
+cd /
